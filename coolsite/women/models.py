6b77cef7eaf9bbd98181034c747b459c660ab4ed
Kostes <qaz1234567890838@gmail.com>
2023-11-08 09:54:54 +0300

    1. Создать таблицу любимых книг с полем их стоимости в издательстве

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+import datetime
 # хранение ORM-моделей для представления данных из БД
+
+dt_now = datetime.datetime.now()
 
 class Student(models.Model):
     fio = models.CharField(max_length=30)
     interesting = models.TextField(blank=True)
+    birdDay = models.DateTimeField(default=dt_now)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_money = models.BooleanField(default=True)
