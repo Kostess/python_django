2c80cae577219341e43197ea476fc19a87682333
Kostess <qaz1234567890838@gmail.com>
2023-11-22 03:54:23 +0300

    Готово

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -12,15 +12,29 @@ class Student(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_money = models.BooleanField(default=True)
 
-def __str__(self):
-    return self.fio
+    def __str__(self):
+        return self.fio
 
 """
     создание записей Student.objects.create(fio="Асадов Наил", interesting="конный спорт, литература, музыка")
     выбор всех записей Student.objects.all()
     выбор записей по критерию Student.objects.filter()
-    выбор одной записи Student.objects.get()
-    выбор 
+    выбор одной записи Student.objects.get() 
+ 
+
+wq = Book.objects.filter(yearPublication__lt=2023)
+
+for i in wq:
+    print(i)
+ 
+ 
+ 
+ws = Book.objects.order_by('price')  
+ 
+for i in ws:
+     print(i)
+
+
 """
 
 class Book(models.Model):
